02f4d77e42c19c342eaa1b4aba68b8520a0bc8e7
Gamin_Grachevskaya <123610071+venator-duplex@users.noreply.github.com>
2026-09-23 12:29:07 +0800

    Add KSU handling for statx in fs/stat.c

--- fs/stat.c
+++ fs/stat.c
@@ -170,6 +170,10 @@ int vfs_statx(int dfd, const char __user *filename, int flags,
 	int error = -EINVAL;
 	unsigned int lookup_flags = LOOKUP_FOLLOW | LOOKUP_AUTOMOUNT;
 
+	#ifdef CONFIG_KSU
+			ksu_handle_statx(&dfd, &filename, &flags, &stat);
+	#endif
+
 	if ((flags & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
 		       AT_EMPTY_PATH | KSTAT_QUERY_FLAGS)) != 0)
 		return -EINVAL;
