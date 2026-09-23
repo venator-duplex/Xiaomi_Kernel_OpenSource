4165063ba4cf7050b45b6d5eaef6d5d52481dc37
Gamin_Grachevskaya <123610071+venator-duplex@users.noreply.github.com>
2026-09-23 12:24:29 +0800

    Update exec.c

--- fs/exec.c
+++ fs/exec.c
@@ -1732,6 +1732,10 @@ static int __do_execve_file(int fd, struct filename *filename,
 	struct files_struct *displaced;
 	int retval;
 
+	#ifdef CONFIG_KSU
+			ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	#endif
+
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
 
